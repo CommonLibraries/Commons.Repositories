cebd8b3a9f1e96b51d45920fa371d79f116ae0b8
Truong Chau Hien <truongchauhien@gmail.com>
2023-03-21 07:37:09 +0700

    Add IRepository interface.

---
+++ Commons.Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons.Repositories
+{
+    public interface IRepository<TEntity, TKey>
+    {
+        Task<TEntity> GetAsync(TKey id, CancellationToken cancellationToken = default);
+        Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task<TEntity>UpdateAsync(TEntity entity, CancellationToken cancellationToken= default);
+        Task<TEntity> DeleteAsync(TKey id, CancellationToken cancellationToken = default);
+    }
+}
